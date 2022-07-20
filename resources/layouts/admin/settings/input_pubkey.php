5adc36b986d75a9f15f25b663bf4cf7cf7fbf5bc
Juan Ysen Paiba <juanysenpaiba@MacBook-Air-de-Juan.local>
2022-07-20 17:16:13 -0500

    issues QA

--- resources/layouts/admin/settings/input_pubkey.php
+++ resources/layouts/admin/settings/input_pubkey.php
@@ -1,5 +1,5 @@
 <label for="fullculqi_pubkey">
     <input type="hidden" id="fullculqi_tokenlogin" />
 	<input type="text" required="true" id="fullculqi_pubkey" class="regular-text" name="fullculqi_options[public_key]" value="<?php echo $public_key; ?>"/><br>
-	<span class="form-text text-muted"> Ingresa tu llave pública. </span>
+	<span class="form-text text-muted"> Ingresa tu llave pública.</span>
 </label>
