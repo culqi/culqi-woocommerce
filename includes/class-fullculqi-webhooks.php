a62648af298221ee2962b5ff6de08be1a2302ff6
JoseHCalderon <105760917+JoseHCalderon@users.noreply.github.com>
2022-09-11 01:47:30 -0500

    val webhook

--- includes/class-fullculqi-webhooks.php
+++ includes/class-fullculqi-webhooks.php
@@ -46,12 +46,15 @@ class FullCulqi_Webhooks {
 
 		switch( $input->type ) {
             case 'order.status.changed' :
-				if (empty($data->currency_code) || empty($data->state)) {
-					exit("Error: currency_code o state incorrectos");
+				if (empty($data->id) || empty($data->order_number) || empty($data->currency_code) || empty($data->state)) {
+					exit("Error: order_id, order_number, currency_code o state vacios");
 				}
                 FullCulqi_Orders::update($data);
                 break;
             case 'refund.creation.succeeded' :
+				if (empty($data->chargeId)) {
+					exit("Error: No envió el chargeId");
+				}
                 $order_id = fullculqi_post_from_meta('_culqi_charge_id', $data->chargeId);
                 $charge_id = fullculqi_post_from_meta('culqi_id', $data->chargeId);
 
@@ -71,7 +74,7 @@ class FullCulqi_Webhooks {
 
                 $order->update_status('refunded',
                     sprintf(
-                        esc_html__('Status changed by FullCulqi (to %s)', 'fullculqi'),'refund'
+                        esc_html__('Status changed by Culqi (to %s)', 'fullculqi'),'refund'
                     )
                 );
                 fullculqi_update_post_meta('culqi_status', $charge_id, 'refunded');
