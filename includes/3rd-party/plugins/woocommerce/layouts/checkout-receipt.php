0763bade356e21196557e90ea861cd7b6f6f40fc
JoseHCalderon <105760917+JoseHCalderon@users.noreply.github.com>
2022-11-11 16:49:17 -0500

    img

--- includes/3rd-party/plugins/woocommerce/layouts/checkout-receipt.php
+++ includes/3rd-party/plugins/woocommerce/layouts/checkout-receipt.php
@@ -1,5 +1,5 @@
 <div id="fullculqi_receipt_page">
-	<img src="<?php echo esc_html($src_image); ?>" alt="fullculqi" />
+	<img width="400px" src="<?php echo esc_html($src_image); ?>" alt="fullculqi" />
 	<br />
 	<p>
 		<?php esc_html_e( 'Make the purchase clicking the ', 'fullculqi'); ?><strong><?php esc_html_e( 'pay button', 'fullculqi'); ?></strong>
