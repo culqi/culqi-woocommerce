5d2abfaa8fef145129a55801c30f74b6ff22f90d
JoseHCalderon <105760917+JoseHCalderon@users.noreply.github.com>
2023-08-10 19:28:59 -0500

    v3.0.5

--- index.php
+++ index.php
@@ -22,7 +22,7 @@ WC tested up to:
 if ( ! defined( 'WPINC' ) ) {
 	die;
 }
-define( 'MPCULQI_PLUGIN_VERSION' , 'v3.0.4');
+define( 'MPCULQI_PLUGIN_VERSION' , 'v3.0.5');
 
 define( 'MPCULQI_FILE' , __FILE__ );
 define( 'MPCULQI_DIR' , plugin_dir_path(__FILE__) );
