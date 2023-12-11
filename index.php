eeb124e486a9c59d203955cf36709c19bf30c234
Jordan Diaz Diaz <jordandiaz2016@gmail.com>
2023-12-10 21:56:00 -0500

    Declare HPOS compatibility

--- index.php
+++ index.php
@@ -105,4 +105,13 @@ function fullculqi() {
 }
 
 $fullculqi = fullculqi();
+
+/* Declare HPOS compatibility */
+add_action( 'before_woocommerce_init', function() {
+if ( class_exists( \Automattic\WooCommerce\Utilities\FeaturesUtil::class ) ) {
+\Automattic\WooCommerce\Utilities\FeaturesUtil::declare_compatibility( 'custom_order_tables', __FILE__, true );
+}
+} );
+
 ?>
+
