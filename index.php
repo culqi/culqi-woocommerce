abd7620bdffd5b2fe128ad133973aa3df2dc43a6
Jordan Diaz <jordandiaz@MacBook-Pro-de-Jordan.local>
2023-12-29 09:42:24 -0500

    se agrego compatibilidad con hpo

--- index.php
+++ index.php
@@ -104,5 +104,13 @@ function fullculqi() {
 	return FullCulqi::instance();
 }
 
+
+/* Declare HPOS compatibility */
+add_action( 'before_woocommerce_init', function() {
+	if ( class_exists( \Automattic\WooCommerce\Utilities\FeaturesUtil::class ) ) {
+	\Automattic\WooCommerce\Utilities\FeaturesUtil::declare_compatibility( 'custom_order_tables', __FILE__, true );
+	}
+	} );
+
 $fullculqi = fullculqi();
 ?>
