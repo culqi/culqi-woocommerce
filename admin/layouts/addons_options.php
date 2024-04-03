839d926460ae54a3decaafc524b7e3823ba5a31d
Jose Deyvi Atachagua Bernaldo <proveedor438@culqi.com>
2024-04-02 19:37:49 -0500

    fix G1-H1 WordPress.Security.EscapeOutput.

--- admin/layouts/addons_options.php
+++ admin/layouts/addons_options.php
@@ -6,17 +6,17 @@
 		<div class="fullculqi_addons_container">
 			<div class="fullculqi_addons_item">
 				<div class="fullculqi_addons_header">
-					<img src="<?php echo MPCULQI_PLUGIN_URL; ?>admin/assets/images/letsgo_1.png" alt="Fullculqi One Click" />
+					<img src="<?php echo esc_url( MPCULQI_PLUGIN_URL . 'admin/assets/images/letsgo_1.png' ); ?>" alt="Fullculqi One Click" />
 				</div>
 				<div class="fullculqi_addons_body">
-					<img src="<?php echo MPCULQI_PLUGIN_URL; ?>admin/assets/images/icon_woo.png" alt="wordpress" />
+					<img src="<?php echo esc_url( MPCULQI_PLUGIN_URL . 'admin/assets/images/icon_woo.png' ); ?>" alt="wordpress" />
 					<h2><?php esc_html_e('Culqi One Click Payments', 'culqi'); ?></h2>
 					<p><?php esc_html_e('Your buyers will can do their purchase with a single click in the checkout page', 'culqi'); ?></p>
 				</div>
 				<div class="fullculqi_addons_footer">
 					<?php if( is_plugin_active('wp-culqi-integration-creditcard/index.php') ) : ?>
 						<a href="https://bit.ly/375PcbS" target="_blank" class="button">
-							<img src="<?php echo admin_url('images/yes.png'); ?>" alt="check" style="vertical-align: middle" />
+							<img src="<?php echo esc_url( admin_url( 'images/yes.png' ) ); ?>" alt="check" style="vertical-align: middle" />
 							<?php esc_html_e('Installed','culqi'); ?>
 						</a>
 					<?php else : ?>
@@ -29,17 +29,17 @@
 		<div class="fullculqi_addons_container">
 			<div class="fullculqi_addons_item">
 				<div class="fullculqi_addons_header">
-					<img src="<?php echo MPCULQI_PLUGIN_URL; ?>admin/assets/images/letsgo_2.png" alt="Woocommerce Culqi Subscriptions" />
+					<img src="<?php echo esc_url( MPCULQI_PLUGIN_URL . 'admin/assets/images/letsgo_2.png' ); ?>" alt="Woocommerce Culqi Subscriptions" />
 				</div>
 				<div class="fullculqi_addons_body">
-					<img src="<?php echo MPCULQI_PLUGIN_URL; ?>admin/assets/images/icon_woo.png" alt="woocommerce" />
+					<img src="<?php echo esc_url( MPCULQI_PLUGIN_URL . 'admin/assets/images/icon_woo.png' ); ?>" alt="woocommerce" />
 					<h2><?php esc_html_e('Culqi Subscriptions', 'culqi'); ?></h2>
 					<p><?php esc_html_e('Your ecommerce will can sell products or services using Culqi recurring payment.', 'culqi'); ?></p>
 				</div>
 				<div class="fullculqi_addons_footer">
 					<?php if( is_plugin_active('wp-culqi-integration-subscription/index.php') ) : ?>
 						<a href="https://bit.ly/2Y8qoMi" target="_blank" class="button">
-							<img src="<?php echo admin_url('images/yes.png'); ?>" alt="check" style="vertical-align: middle" />
+							<img src="<?php echo esc_url( admin_url( 'images/yes.png' ) ); ?>" alt="check" style="vertical-align: middle" />
 							<?php esc_html_e('Installed','culqi'); ?>
 						</a>
 					<?php else : ?>
@@ -52,17 +52,17 @@
 		<div class="fullculqi_addons_container">
 			<div class="fullculqi_addons_item">
 				<div class="fullculqi_addons_header">
-					<img src="<?php echo MPCULQI_PLUGIN_URL; ?>admin/assets/images/letsgo_3.png" alt="Wordpress Culqi Payment Buttons" />
+					<img src="<?php echo esc_url( MPCULQI_PLUGIN_URL . 'admin/assets/images/letsgo_3.png' ); ?>" alt="Wordpress Culqi Payment Buttons" />
 				</div>
 				<div class="fullculqi_addons_body">
-					<img src="<?php echo MPCULQI_PLUGIN_URL; ?>admin/assets/images/icon_wp.png" alt="wordpress" />
+					<img src="<?php echo esc_url( MPCULQI_PLUGIN_URL . 'admin/assets/images/icon_wp.png' ); ?>" alt="wordpress" />
 					<h2><?php esc_html_e('Culqi Payment Buttons', 'culqi'); ?></h2>
 					<p><?php esc_html_e('You can use Culqi payments buttons in everywhere of you website without have a ecommerce installed.', 'culqi'); ?></p>
 				</div>
 				<div class="fullculqi_addons_footer">
 					<?php if( is_plugin_active('wp-culqi-integration-button/index.php') ) : ?>
 						<a href="https://bit.ly/3dMEpGc" target="_blank" class="button">
-							<img src="<?php echo admin_url('images/yes.png'); ?>" alt="check" style="vertical-align: middle" />
+							<img src="<?php echo esc_url( admin_url( 'images/yes.png' ) ); ?>" alt="check" style="vertical-align: middle" />
 							<?php esc_html_e('Installed','culqi'); ?>
 						</a>
 					<?php else : ?>
@@ -77,17 +77,17 @@
 		<div class="fullculqi_addons_container">
 			<div class="fullculqi_addons_item">
 				<div class="fullculqi_addons_header">
-					<img src="<?php echo MPCULQI_PLUGIN_URL; ?>admin/assets/images/letsgo_4.png" alt="Wordpress Culqi Deferred Payment" />
+					<img src="<?php echo esc_url( MPCULQI_PLUGIN_URL . 'admin/assets/images/letsgo_4.png' ); ?>" alt="Wordpress Culqi Deferred Payment" />
 				</div>
 				<div class="fullculqi_addons_body">
-					<img src="<?php echo MPCULQI_PLUGIN_URL; ?>admin/assets/images/icon_woo.png" alt="woocommerce" />
+					<img src="<?php echo esc_url( MPCULQI_PLUGIN_URL . 'admin/assets/images/icon_woo.png' ); ?>" alt="woocommerce" />
 					<h2><?php esc_html_e('Culqi Deferred Payment', 'culqi'); ?></h2>
 					<p><?php esc_html_e('You can authorize a payment, check its stocks, and then capture the payment.', 'culqi'); ?></p>
 				</div>
 				<div class="fullculqi_addons_footer">
 					<?php if( is_plugin_active('wp-culqi-integration-capture/index.php') ) : ?>
 						<a href="https://bit.ly/2Mye7v8" target="_blank" class="button">
-							<img src="<?php echo admin_url('images/yes.png'); ?>" alt="check" style="vertical-align: middle" />
+							<img src="<?php echo esc_url( admin_url( 'images/yes.png' ) ); ?>" alt="check" style="vertical-align: middle" />
 							<?php esc_html_e('Installed','culqi'); ?>
 						</a>
 					<?php else : ?>
