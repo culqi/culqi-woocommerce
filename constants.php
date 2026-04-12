c8f6f16980404cd18a21c890b7f59314ac10fa43
JoseHCalderon <105760917+JoseHCalderon@users.noreply.github.com>
2026-04-11 20:59:55 -0500

    v4.0.1

--- constants.php
+++ constants.php
@@ -7,7 +7,7 @@ define( 'PLUGIN_CULQI_URL' , plugin_dir_url(__FILE__) );
 define( 'CULQI_API_URL' , 'https://ag-online.culqi.com/gateway/' );
 define( 'CULQI_CONFIG_URL' , 'https://configonlineplatform.culqi.com' );
 define( 'EXPIRATION_TIME' , 24 );
-define( 'PLUGIN_VERSION', 'v4.0.0');
+define( 'PLUGIN_VERSION', 'v4.0.1');
 define( 'PROCESSING', 'processing');
 define( 'PLATFORM', 'woocommerce');
 define( 'TEST', 'test');
