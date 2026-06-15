c78c1ebcac1af2bb656c58753b13a8a77288ca40
Jorge Estrada <proveedor824@culqi.com>
2026-06-15 17:46:46 -0500

    feat(CNP-3921): update version

--- constants.php
+++ constants.php
@@ -11,7 +11,7 @@ define( 'CULQI_API_URL' , 'https://ag-online.culqi.com/gateway/' );
 define( 'CULQI_CONFIG_URL' , 'https://configonlineplatform.culqi.com' );
 
 define( 'EXPIRATION_TIME' , 24 );
-define( 'PLUGIN_VERSION', 'v4.0.1');
+define( 'PLUGIN_VERSION', 'v4.1.0');
 define( 'PROCESSING', 'processing');
 define( 'PLATFORM', 'woocommerce');
 define( 'TEST', 'test');
